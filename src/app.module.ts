fdd21a0204ae04e1713c984262c9479467e4821f
abusayadhussain <abusayadhussain@gmail.com>
2024-08-21 19:41:11 +0600

    design folder structure for the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MachineModule } from './machine/machine.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [MachineModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
